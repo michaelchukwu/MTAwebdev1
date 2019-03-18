0e794546a2ef80439969170f28ede86773106937
Mercy Nwachukwu <mercynwachukwu70@gmail.com>
2019-03-18 12:47:01 +0100

    made corrections to html document

---
+++ mercy/group project/main.js
@@ -0,0 +1,3 @@
+// the survey should take on a secton per page i.e the other section not been answered should be hidden 
+//a section ends with the next button 
+// study study and dont change the html document 
